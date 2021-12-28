b11a926d79f1e2a7712cd9bbdefea8056a67ae76
Leandro Braga <leandro.braga@ifood.com.br>
2021-12-28 18:09:57 -0300

    Organizing CLI in subcommands.

--- src/bin.rs
+++ src/bin.rs
@@ -1,4 +1,4 @@
-use structopt::{clap::arg_enum, StructOpt};
+use structopt::StructOpt;
 
 fn main() {
     let command = Arguments::from_args();
@@ -8,25 +8,24 @@ fn main() {
 #[derive(Debug, StructOpt)]
 #[structopt(name = "Stocks", about = "A simple CLI to manage stocks.")]
 struct Arguments {
-    #[structopt(
-        name="COMMAND",
-        possible_values=&Command::variants(),
-        case_insensitive=true, help="The command that will be executed."
-    )]
+    #[structopt(subcommand)]
     command: Command,
-    #[structopt(name = "SYMBOL", help = "The Stock ticker (e.g. BBAS3).")]
-    symbol: String,
-    #[structopt(
-        name = "VALUE",
-        help = "How much it is going to be bought or sold (e.g. 100)."
-    )]
-    value: u32,
 }
 
-arg_enum! {
-    #[derive(Debug)]
-    enum Command {
-        Buy,
-        Sell,
-    }
+#[derive(Debug, StructOpt)]
+enum Command {
+    #[structopt(about = "Buys a stock.")]
+    Buy {
+        #[structopt(name = "SYMBOL", help = "The Stock ticker (e.g. BBAS3).")]
+        symbol: String,
+        #[structopt(name = "VALUE", help = "How much it is going to be bought (e.g. 100).")]
+        quantity: u32,
+    },
+    #[structopt(about = "Sells a stock.")]
+    Sell {
+        #[structopt(name = "SYMBOL", help = "The Stock ticker (e.g. BBAS3).")]
+        symbol: String,
+        #[structopt(name = "VALUE", help = "How much it is going to be sold (e.g. 100).")]
+        quantity: u32,
+    },
 }
