e1078349d7f6b3afd1f472751733af5b41dcf4ca
Leandro Braga <leandrobbraga@gmail.com>
2023-03-30 16:39:36 -0300

    Avoid heap allocations during logging

--- src/bin/cli/log.rs
+++ src/bin/cli/log.rs
@@ -2,20 +2,20 @@
 #[macro_export]
 macro_rules! error {
     ($($arg:tt)*) => {
-        eprintln!("\x1b[91mERROR\x1b[0m: {}", format!($($arg)*))
+        eprintln!("\x1b[91mERROR\x1b[0m: {}", std::format_args!($($arg)*))
     };
 }
 
 #[macro_export]
 macro_rules! info {
     ($($arg:tt)*) => {
-        eprintln!("\x1b[92mINFO\x1b[0m: {}", format!($($arg)*))
+        eprintln!("\x1b[92mINFO\x1b[0m: {}", std::format_args!($($arg)*))
     };
 }
 
 #[macro_export]
 macro_rules! warn {
     ($($arg:tt)*) => {
-        eprintln!("\x1b[93mWARNING\x1b[0m: {}", format!($($arg)*))
+        eprintln!("\x1b[93mWARNING\x1b[0m: {}", std::format_args!($($arg)*))
     };
 }
